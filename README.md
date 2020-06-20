a98b33ac6b456ab3b75be2113664d894b262c093
victormmp <victormmp@outlook.com>
2020-06-20 20:51:55 -0300

    Add badge to README file and fix name of test task on CI workflow

---
+++ README.md
@@ -0,0 +1,5 @@
+![Python application](https://github.com/victormmp/graph_em/workflows/Python%20application/badge.svg)
+
+# Graph EM
+
+Expectation-Maximization based Gaussian Mixture classifier with graph sample initialization.
--- graph_em/graph/sampled.py
+++ graph_em/graph/sampled.py
@@ -4,7 +4,7 @@ from .base import SimpleUndirectedGraph
 
 class SampledGraph(SimpleUndirectedGraph):
 
-    def __init__(self, points: np.ndarray):
+    def __init__(self, points: np.ndarray, fraction: int = 0.1):
 
-
+        points = np.random.choice()
         super(SampledGraph, self).__init__(points=points)
